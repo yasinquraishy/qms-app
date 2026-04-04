ef9da8ed6b25aa9dc8ded3d60fd15aa5d19e45af
Yasin Quraishy <yasin.quraishy@we45.com>
2026-04-04 06:07:12 +0530

    refactor: improve code consistency and readability in index.js

--- syncEngine/sw/src/index.js
+++ syncEngine/sw/src/index.js
@@ -1,40 +1,54 @@
-import { MSG } from "../../shared/messageTypes.js";
-import { IndexedDB } from "../../shared/IndexedDB.js";
-import { loadMetas } from "./metaLoader.js";
-import { startPolling, stopPolling } from "./mutationPoller.js";
-import { connectSocket, disconnectSocket } from "./socketHandler.js";
+import { MSG } from '../../shared/messageTypes.js'
+import { IndexedDB } from '../../shared/IndexedDB.js'
+import { loadMetas } from './metaLoader.js'
+import { startPolling, stopPolling } from './mutationPoller.js'
+import { connectSocket, disconnectSocket } from './socketHandler.js'
 
-let config = null;
-let metas = { metaMap: new Map(), metaByTable: new Map() };
+let config = null
+let metas = { metaMap: new Map(), metaByTable: new Map() }
 
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
+async function hasActiveClients() {
+  const clientsList = await self.clients.matchAll({ type: 'window' })
+  return clientsList.length > 0
+}
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+setInterval(async () => {
+  const active = await hasActiveClients()
+  if (!active) {
+    console.log('No main threads connected, stopping...')
+    stopPolling()
+    disconnectSocket()
+  }
+}, 50000)
 
-self.addEventListener("message", async (event) => {
-  const { type, ...payload } = event.data;
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('message', async (event) => {
+  const { type, ...payload } = event.data
 
   switch (type) {
     case MSG.INIT: {
-      config = payload;
-      await IndexedDB.init(config.dbName);
-      metas = await loadMetas();
-      startPolling(metas.metaMap, config);
-      if (config.socketUrl) connectSocket(config, metas.metaByTable);
-      break;
+      config = payload
+      await IndexedDB.init(config.dbName)
+      metas = await loadMetas()
+      startPolling(metas.metaMap, config)
+      if (config.socketUrl) connectSocket(config, metas.metaByTable)
+      break
     }
     case MSG.STOP: {
-      stopPolling();
-      disconnectSocket();
-      break;
+      stopPolling()
+      disconnectSocket()
+      break
     }
     case MSG.REFRESH_METAS: {
-      metas = await loadMetas();
-      break;
+      metas = await loadMetas()
+      break
     }
   }
-});
+})
